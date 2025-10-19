4c56f666c175cfe496d349f7cdb928a3ea4afa2b
Md Badrul Alam <developer.badrul@gmail.com>
2025-10-19 16:01:36 +0600

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <p className="text-3xl font-bold underline">This is my app</p>
+        
+    </>
+  )
+}
+
+export default App
