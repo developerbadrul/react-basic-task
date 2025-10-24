8266f949fcea322de4325dbf41728db4babd29dc
Md Badrul Alam <developer.badrul@gmail.com>
2025-10-25 01:54:28 +0600

    add all delete feature

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -52,6 +52,10 @@ const Table = () => {
         const tasksAfterDelete = tasks.filter(task => task.id !== taskId);
         setTasks(tasksAfterDelete)
     }
+
+    const handleDeleteAllClick = () => {
+        setTasks([]);
+    }
     return (
         <section className="mb-20" id="tasks">
             {showModal && (
@@ -68,6 +72,7 @@ const Table = () => {
                 <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
                     <TableActions
                         onAddClick={() => setShowModal(true)}
+                        onDeleteAllClick={handleDeleteAllClick}
                     />
                     <TableList
                         tasks={tasks}
